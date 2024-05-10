6115874c497aa2fcaee5a3162ee013c6f1ff3c8c
MartinXu <20XuPM@tonbridge-school.org>
2024-05-10 12:05:39 +0100

    Created buttons

--- GUI.py
+++ GUI.py
@@ -1,9 +1,17 @@
 import tkinter as tk
 
+
+def new_window():
+    window.destroy()
+    window1 = tk.Tk()
+    window1.geometry("400x500")
+    window1.mainloop()
+
+
 window = tk.Tk()
 window.geometry("400x500")
-start = tk.Button(text="start", bg="red",height="5",width="10")
-quit = tk.Button(window, text="quit",bg="blue",height="5",width="10",command=window.destroy)
+start = tk.Button(text="START", bg="red", height="5", width="10", command=new_window)
+Quit = tk.Button(window, text="QUIT", bg="blue", height="5", width="10", command=window.destroy)
 start.pack()
-quit.pack()
+Quit.pack()
 window.mainloop()
