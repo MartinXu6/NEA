4054eb7569a1420119719937130d0f6ebc6a4dff
MartinXu <20XuPM@tonbridge-school.org>
2024-09-19 08:29:43 +0100

    .

--- iug.py
+++ iug.py
@@ -2,8 +2,9 @@ from tkinter import *
 
 root = Tk()
 root.title("ZERO POINT ONE")
-root.geometry("800x600")
+root.geometry("1500x800")
 root.resizable(False, False)
+canvas = Canvas()
 
 
 def on_click(i, j, event):
@@ -18,15 +19,21 @@ board = [[0, 0, 0, 0, 0, 0, 0, 0],
          [0, 0, 0, 0, 0, 0, 0, 0],
          [0, 0, 0, 0, 0, 0, 0, 0],
          [0, 0, 0, 0, 0, 0, 0, 0], ]
-counter = 0
+main_board = Frame(root, bg="black", bd=0, width=440, height=440)
+main_board.place(relx=0.5, y=370, anchor="center")
 for i in range(8):
     for j in range(8):
-        if counter %2 ==0 :
-            square = Label(root, height=4, width=8, bg="red")
-            counter += 1
+        if 6 <= i <= 7:
+            square = Frame(main_board, height=55, width=55, bg="red")
+        elif 0 <= i <= 1:
+            square = Frame(main_board, height=55, width=55, bg="blue")
         else:
-            square = Label(root, height=4, width=8, bg="black")
-            counter += 1
+            square = Frame(main_board, height=55, width=55, bg="white")
         square.bind("<Button-1>", lambda e, i=i, j=j: on_click(i, j, e))
-        square.place(x=i * 50 + 185, y=j * 50+85)
+        square.grid(row=i, column=j, padx=1, pady=1)
+red_pieces = Frame(root, bg="brown", width=500, height=100, )
+red_pieces.place(relx=0.5, y=680, anchor="center")
+blue_pieces = Frame(root, bg="purple", width=500, height=100, )
+blue_pieces.place(relx=0.5, y=60, anchor="center")
+canvas.pack()
 root.mainloop()
